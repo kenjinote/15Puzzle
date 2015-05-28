767464a074f331890d61385c1aed4a23c1c0d11e
hack-jp <kenji256@gmail.com>
2015-05-28 11:30:06 +0900

    Add files

---
+++ main.cpp
@@ -0,0 +1,248 @@
+#define UNICODE
+#pragma comment(linker,"/opt:nowin98")
+#include<windows.h>
+#include"resource.h"
+
+TCHAR szClassName[]=TEXT("Window");
+
+#define BITMAP_WIDTH 100
+#define BITMAP_HEIGHT 99
+#define WM_CLICK (WM_APP+100)
+#define WM_RANDOM (WM_APP+101)
+
+HHOOK g_hHook;
+
+LRESULT CALLBACK CBTProc(int nCode,WPARAM wParam,LPARAM lParam)
+{
+    switch(nCode)
+	{
+    case HCBT_ACTIVATE:
+        {
+            UnhookWindowsHookEx(g_hHook);
+            HWND hMes=(HWND)wParam;
+			HWND hWnd=GetParent(hMes);
+            RECT m,w;
+            GetWindowRect(hMes,&m);
+            GetWindowRect(hWnd,&w);
+            SetWindowPos(
+				hMes,
+				hWnd,
+				(w.right+w.left-m.right+m.left)/2,
+				(w.bottom+w.top-m.bottom+m.top)/2,
+				0,
+				0,
+				SWP_NOSIZE|SWP_NOZORDER|SWP_NOACTIVATE
+				);
+        }
+    }
+    return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd,UINT msg,WPARAM wParam,LPARAM lParam)
+{
+	static int table[16];
+	static HBITMAP hBitmap[15];
+	int i,j,index;
+	PAINTSTRUCT ps;
+	HDC hdc,hdc_mem;
+	switch(msg)
+	{
+	case WM_CREATE:
+		for(i=0;i<15;i++)
+		{
+			table[i]=i;
+			hBitmap[i]=LoadBitmap(
+				((LPCREATESTRUCT)lParam)->hInstance,
+				MAKEINTRESOURCE(IDB_BITMAP1+i)
+				);
+		}
+		table[15]=15;
+		srand(GetTickCount());
+		SendMessage(hWnd,WM_RANDOM,0,0);
+		break;
+	case WM_RANDOM:
+		for(i=0;i<1000;i++)
+			SendMessage(hWnd,WM_CLICK,rand()%16,0);
+		InvalidateRect(hWnd,0,0);
+		break;
+	case WM_PAINT:
+		hdc=BeginPaint(hWnd,&ps);
+		hdc_mem=CreateCompatibleDC(hdc);
+		for(i=0;i<16;i++)
+		{
+			if(table[i]==15)
+			{
+				RECT rect;
+				SetRect(
+					&rect,
+					i%4*BITMAP_WIDTH,
+					i/4*BITMAP_HEIGHT,
+					i%4*BITMAP_WIDTH+BITMAP_WIDTH,
+					i/4*BITMAP_WIDTH+BITMAP_HEIGHT
+					);
+				ExtTextOut(hdc,0,0,ETO_OPAQUE,&rect,0,0,0);
+			}
+			else
+			{
+				SelectObject(
+					hdc_mem,
+					hBitmap[table[i]]
+					);
+				BitBlt(
+					hdc,
+					i%4*BITMAP_WIDTH,
+					i/4*BITMAP_HEIGHT,
+					BITMAP_WIDTH,
+					BITMAP_HEIGHT,
+					hdc_mem,
+					0,
+					0,
+					SRCCOPY
+					);
+			}
+		}
+		DeleteDC(hdc_mem);
+		EndPaint(hWnd,&ps);
+		break;
+	case WM_LBUTTONDOWN:
+		SendMessage(
+			hWnd,
+			WM_CLICK,
+			LOWORD(lParam)/BITMAP_WIDTH+HIWORD(lParam)/BITMAP_HEIGHT*4,
+			1
+			);
+		break;
+	case WM_KEYDOWN:
+		for(i=0;i<16;i++)
+			if(table[i]==15)
+				break;
+        switch (wParam) 
+        { 
+		case VK_LEFT:
+			SendMessage(hWnd,WM_CLICK,i+1,1);
+			break; 
+		case VK_RIGHT: 
+			SendMessage(hWnd,WM_CLICK,i-1,1);
+			break; 
+		case VK_UP: 
+			SendMessage(hWnd,WM_CLICK,i+4,1);
+			break; 
+		case VK_DOWN: 
+			SendMessage(hWnd,WM_CLICK,i-4,1);
+			break; 
+		}
+		break;
+	case WM_CLICK:
+		index=(int)wParam;
+		if(table[index]==15||index<0||index>15)
+			break;
+		for(i=0;i<4;i++)
+		{
+			if(index%4>i&&table[index-(i+1)]==15)//move left
+			{
+				for(j=i;j>=0;j--)table[index-(j+1)]=table[index-j];
+				table[index]=15;
+				break;
+			}
+			else if(index>3+4*i&&table[index-4*(i+1)]==15)//move up
+			{
+				for(j=i;j>=0;j--)table[index-4*(j+1)]=table[index-4*j];
+				table[index]=15;
+				break;
+			}
+			else if(index%4<3-i&&table[index+(i+1)]==15)//move right
+			{
+				for(j=i;j>=0;j--)table[index+(j+1)]=table[index+j];
+				table[index]=15;
+				break;
+			}
+			else if(index<12-i*4&&table[index+4*(i+1)]==15)//move down
+			{
+				for(j=i;j>=0;j--)table[index+4*(j+1)]=table[index+4*j];
+				table[index]=15;
+				break;
+			}
+		}
+		if(lParam&&table[index]==15)
+		{
+			InvalidateRect(hWnd,0,0);
+			if(index==15)
+			{
+				for(i=0;i<16;i++)
+					if(table[i]!=i)
+						return 0;
+				g_hHook=SetWindowsHookEx(
+					WH_CBT,
+					CBTProc,
+					NULL,
+					GetCurrentThreadId()
+					);
+				if(IDYES==MessageBox(
+					hWnd,
+					TEXT("‚à‚¤ˆê“xs‚¢‚Ü‚·‚©?"),
+					TEXT("Š®¬!"),
+					MB_YESNO)
+					)
+					SendMessage(hWnd,WM_RANDOM,0,0);
+			}
+		}
+		break;
+	case WM_DESTROY:
+		for(i=0;i<15;i++)
+		{
+			DeleteObject(hBitmap[i]);
+		}
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd,msg,wParam,lParam);
+	}
+	return 0;
+}
+
+EXTERN_C void __cdecl WinMainCRTStartup()
+{
+	MSG msg;
+	HINSTANCE hInstance=GetModuleHandle(0);
+	WNDCLASS wndclass={
+		0,
+			WndProc,
+			0,
+			0,
+			hInstance,
+			0,
+			LoadCursor(0,IDC_ARROW),
+			0,
+			0,
+			szClassName
+	};
+	RegisterClass(&wndclass);
+	RECT rect;
+	SetRect(&rect,0,0,BITMAP_WIDTH*4,BITMAP_HEIGHT*4);
+	AdjustWindowRect(&rect,WS_OVERLAPPED|WS_CAPTION|WS_SYSMENU,0);
+	HWND hWnd=CreateWindow(
+		szClassName,
+		TEXT("15 Puzzle"),
+		WS_OVERLAPPED|WS_CAPTION|WS_SYSMENU,
+		CW_USEDEFAULT,
+		0,
+		rect.right-rect.left,
+		rect.bottom-rect.top,
+		0,
+		0,
+		hInstance,
+		0
+		);
+	ShowWindow(hWnd,SW_SHOWDEFAULT);
+	UpdateWindow(hWnd);
+	while(GetMessage(&msg,0,0,0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	ExitProcess(msg.wParam);
+}
+
+#if _DEBUG
+void main(){}
+#endif
---
+++ resource.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by res.rc
+//
+#define IDB_BITMAP1                     101
+#define IDB_BITMAP2                     102
+#define IDB_BITMAP3                     103
+#define IDB_BITMAP4                     104
+#define IDB_BITMAP5                     105
+#define IDB_BITMAP6                     106
+#define IDB_BITMAP7                     107
+#define IDB_BITMAP8                     108
+#define IDB_BITMAP9                     109
+#define IDB_BITMAP10                    110
+#define IDB_BITMAP11                    111
+#define IDB_BITMAP12                    112
+#define IDB_BITMAP13                    113
+#define IDB_BITMAP14                    114
+#define IDB_BITMAP15                    115
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        116
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
